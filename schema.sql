15b7b1d089d7853fa901c0f48d57b496b1560c0a
Bruno Marques <brunomarques@Brunos-MacBook-Pro.local>
2016-05-11 12:56:41 -0400

    schema

--- schema.sql
+++ schema.sql
@@ -1,68 +1,199 @@
-CREATE DATABASE squad_bank;
-USE squad_bank;
+-- phpMyAdmin SQL Dump
+-- version 4.2.12deb2+deb8u1build0.15.04.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 11, 2016 at 12:46 PM
+-- Server version: 5.6.22-71.0
+-- PHP Version: 5.6.4-4ubuntu6.4
 
-CREATE TABLE users (
-  id int AUTO_INCREMENT, 
-  hash id, 
-  name varchar(40) NOT NULL,
-  PRIMARY KEY(id),
-  email varchar(50) NOT NULL, 
-  password char(128) NOT NULL, 
-  fb_token char(200) NOT NULL, 
-  created datetime, 
-  updated datetime, 
-  status, 
-  role enum
-); 
-CREATE TABLE events (
-  id int AUTO_INCREMENT, 
-  hash id, 
-  name varchar(30) NOT NULL,
-  user_id, 
-  created,
-  updated,
-  status, 
-  period int, 
-  picture, 
-  destination 
-  individual_cost
-
-); 
-CREATE TABLE meta_events (
-	id,
-	event id, 
-	keywords, 
-	information
-); 
-
-CREATE TABLE transactions ( 
-	transaction id, 
-	hash id, 
-	event id, 
-	user,
-	description,
-	created,
-); 
-
-CREATE TABLE funding (
-	event id, 
-	userid, 
-	funding_frequency, 
-	auto_debit= yes or no, 
-	money_pull_date 
-	amount 
-	manual_outside_contribution'research api'
-
-) 
-CREATE TABLE withdrawals(
-	event_id, 
-	user_id 
-	withdrawal_amount 
-	withdrawal_date 
-); 
-CREATE TABLE chat( 
-research chat data
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `squad_bank`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `campaigns`
+--
+
+CREATE TABLE IF NOT EXISTS `campaigns` (
+`id` bigint(20) unsigned NOT NULL,
+  `hash_id` char(15) COLLATE utf8_bin NOT NULL,
+  `title` varchar(100) COLLATE utf8_bin NOT NULL,
+  `description` tinytext COLLATE utf8_bin NOT NULL,
+  `campaign_type` enum('one','two','three') COLLATE utf8_bin NOT NULL,
+  `url` varchar(100) COLLATE utf8_bin NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `status` enum('active','inactive') COLLATE utf8_bin NOT NULL,
+  `photo` char(15) COLLATE utf8_bin NOT NULL,
+  `target_amount` decimal(19,4) NOT NULL,
+  `match_amount` enum('0','1') COLLATE utf8_bin NOT NULL,
+  `starts_on` date NOT NULL,
+  `ends_on` date NOT NULL,
+  `period` mediumint(8) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fundings`
+--
+
+CREATE TABLE IF NOT EXISTS `fundings` (
+`id` bigint(20) unsigned NOT NULL,
+  `campaign_id` bigint(20) unsigned NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `frequency` tinyint(3) unsigned NOT NULL,
+  `auto_debit` enum('Y','N') COLLATE utf8_bin NOT NULL,
+  `amount` decimal(19,4) NOT NULL,
+  `lodged_date` datetime NOT NULL,
+  `status` enum('pending','approved','declined') COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `meta_campaigns`
+--
+
+CREATE TABLE IF NOT EXISTS `meta_campaigns` (
+`id` bigint(20) unsigned NOT NULL,
+  `campaign_id` bigint(20) unsigned NOT NULL,
+  `keywords` text COLLATE utf8_bin NOT NULL,
+  `category` varchar(100) COLLATE utf8_bin NOT NULL,
+  `info` text COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pledges`
+--
+
+CREATE TABLE IF NOT EXISTS `pledges` (
+  `campaign_id` bigint(20) unsigned NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `pledge_type` varchar(25) COLLATE utf8_bin NOT NULL,
+  `amount` decimal(19,4) NOT NULL,
+  `status` enum('approved','declined','in_progress') COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+`id` int(10) unsigned NOT NULL,
+  `first_name` varchar(20) COLLATE utf8_bin NOT NULL,
+  `last_name` varchar(20) COLLATE utf8_bin NOT NULL,
+  `email` varchar(40) COLLATE utf8_bin NOT NULL,
+  `password` binary(32) NOT NULL,
+  `facebook_id` bigint(64) NOT NULL,
+  `facebook_access_token` tinytext COLLATE utf8_bin NOT NULL,
+  `role` enum('super_admin','admin','campaign_creator','campaign_viewer') COLLATE utf8_bin NOT NULL,
+  `hash_id` char(15) COLLATE utf8_bin NOT NULL,
+  `created_at` date NOT NULL,
+  `updated_at` date NOT NULL,
+  `profile` char(15) COLLATE utf8_bin NOT NULL,
+  `status` enum('active','inactive') COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `withdrawals`
+--
+
+CREATE TABLE IF NOT EXISTS `withdrawals` (
+`id` bigint(20) unsigned NOT NULL,
+  `campaign_id` bigint(20) unsigned NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `amount` decimal(19,4) NOT NULL,
+  `reason` text COLLATE utf8_bin NOT NULL,
+  `status` enum('approved','declined','in_progress') COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `campaigns`
+--
+ALTER TABLE `campaigns`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `fundings`
+--
+ALTER TABLE `fundings`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `meta_campaigns`
+--
+ALTER TABLE `meta_campaigns`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `withdrawals`
+--
+ALTER TABLE `withdrawals`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `campaigns`
+--
+ALTER TABLE `campaigns`
+MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `fundings`
+--
+ALTER TABLE `fundings`
+MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `meta_campaigns`
+--
+ALTER TABLE `meta_campaigns`
+MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `withdrawals`
+--
+ALTER TABLE `withdrawals`
+MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
 
